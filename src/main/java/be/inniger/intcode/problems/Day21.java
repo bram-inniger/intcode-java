9eaf39ef9700e74188a5d70a7063369ed449a908
Bram Inniger <bram@inniger.be>
2024-10-29 00:57:41 +0200

    Solved D21P01

---
+++ src/main/java/be/inniger/intcode/problems/Day21.java
@@ -0,0 +1,49 @@
+package be.inniger.intcode.problems;
+
+import be.inniger.intcode.IntCode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day21 {
+
+    public static long partOne(List<Long> program) {
+        // Manually worked out on paper and by looking at failure scenarios
+        // If _any_ of the 3 next distances away is a hole AND the 4th one is ground, then jump
+        //
+        // J = ( (NOT A) OR (NOT B) OR (NOT C) ) AND D
+        //
+        // which is equivalent to (De Morgan's Law)
+        //
+        // J = ( NOT ( A AND B AND C ) ) AND D
+        //
+        // Which translates to the jumpscript
+        //
+        //  OR  A T  -> T starts at false, so this stores the current value of A into T
+        //  AND B T  -> store "B AND T" in T -> "T = B AND A"
+        //  AND C T  -> store "C AND T" in T -> "T = C AND B AND A"
+        //  NOT T J  -> store the negation of T in J -> "J = NOT ( C AND B AND A )"
+        //  AND D J  -> store "D and J" in J -> "J = ( NOT ( C AND B AND A ) ) AND D"
+        //  WALK     -> start the program
+        var jumpScript = """
+                OR  A T
+                AND B T
+                AND C T
+                NOT T J
+                AND D J
+                WALK
+                """.chars().mapToLong(c -> (long) c).boxed().toList();
+        var intCode = new IntCode(program);
+
+        jumpScript.forEach(c -> intCode.input().add(c));
+        intCode.run();
+        var output = intCode.output().stream().toList();
+
+        if (output.getLast() <= Character.MAX_VALUE) {
+            var debug = output.stream().map(c -> (char) (long) c).map(Object::toString).collect(Collectors.joining());
+            throw new IllegalStateException(debug);
+        }
+
+        return output.getLast();
+    }
+}
---
+++ src/test/java/be/inniger/intcode/problems/Day21Test.java
@@ -0,0 +1,23 @@
+package be.inniger.intcode.problems;
+
+import be.inniger.intcode.TestUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day21Test {
+
+    private static final List<Long> INPUT = TestUtil.readProgram("21");
+
+    @Test
+    public void partOneSampleTest() {
+        // No sample values given
+    }
+
+    @Test
+    public void partOneActualTest() {
+        assertEquals(19_352_638, Day21.partOne(INPUT));
+    }
+}
